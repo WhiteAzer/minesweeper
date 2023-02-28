f8b3ff52bf9f5056722baf2167edb40de2751624
WhiteAzer <0202timur2020@gmail.com>
2023-02-28 16:09:03 +0300

    init: first commit

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,49 @@
+import { RuleSetRule } from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export const buildLoaders = ( isDev: boolean ): RuleSetRule[] => {
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif|woff|woff2)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  }
+
+  const svgLoader = {
+    test: /\.svg$/,
+    use: [ '@svgr/webpack' ],
+  }
+
+  const tsLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: ( resPath: string ) => resPath.includes( ".module" ),
+            localIdentName: isDev ? "[path][name]__[local]--[hash:base64:5]" : "[hash:base64:8]"
+          }
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+
+  return [
+    fileLoader,
+    svgLoader,
+    tsLoader,
+    scssLoader
+  ]
+}
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./styles/index.scss";
+import { GameField } from "./components/GameField/GameField";
+
+
+export const App = () => {
+  return (<GameField/> );
+}
