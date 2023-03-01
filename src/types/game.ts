0cea6f39d8ce9ce0199017104ff19d8779dbe9d7
WhiteAzer <0202timur2020@gmail.com>
2023-03-01 19:47:40 +0300

    feat: added game types

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,3 +1,7 @@
-export type GameCellState = -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+export type GameCellInnerState = -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
-export type GameMap = Array<GameCellState>;
+export type GameMap = Array<Array<GameCellInnerState>>;
+
+export type GameCellMaskState = "active" | "selected" | "flag" | "guess";
+
+export type GameMask = Array<Array<GameCellMaskState>>;
